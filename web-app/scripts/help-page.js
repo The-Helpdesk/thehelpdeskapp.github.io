201dc92ea8f30a22cf4864be1e2ad2c695655d15
thehelpdeskapp <67212071+thehelpdeskapp@users.noreply.github.com>
2021-02-19 09:47:44 -0500

    Updated Help Page

--- web-app/scripts/help-page.js
+++ web-app/scripts/help-page.js
@@ -1,5 +1,5 @@
 
-var dvc_catogory = ["Mobile","Desktop","Networking"];
+var dvc_catogory = ["Mobile","Desktop","Networking", "Smart TVs","Printers"];
 var chcs = document.getElementsByClassName("optn-btn-ttl");
 var chcArray = dvc_catogory;
 var iteration = 0;
@@ -50,38 +50,69 @@ function findValuesHelper(obj, key, list) {
 }
 if (pass == 'cnt') {
 var selectedBtn = document.getElementById('optn-btn-ttl-' + optnchc).innerHTML;
-toDisplay = findValues(data, selectedBtn);
-keyContent = Object.keys(toDisplay[0][0])
-keyContent = keyContent.sort();
-for (var i=0; i < keyContent.length; i++) {
-document.getElementById('optn-btn-ttl-' + i).innerHTML = keyContent[i];
-}
-var numOfBoxToClear = 10 - keyContent.length;
-for (var i = keyContent.length; i< numOfBoxToClear; i++) {
-document.getElementById('optn-btn-ttl-' + i).innerHTML = '';
-}
-document.getElementById("choice-title").innerHTML = '';
-iteration++
-} else if (pass == 'hover') {
-var hoverBtn = document.getElementById('optn-btn-ttl-' + optnhvrchc).innerHTML;
-document.getElementById("choice-hover-title").innerHTML = hoverBtn;
-toDisplay = findValues(data, hoverBtn);
-keyContent = Object.keys(toDisplay[0][0])
-keyContent = keyContent.sort();
-for (var i = 0; i < keyContent.length; i++) {
-var choiceHoverList = document.getElementById("choice-hover-list");
-if (i > 0) {
-choiceHoverList.innerHTML = choiceHoverList.innerHTML += ', ' + keyContent[i];
-} else {
-choiceHoverList.innerHTML = keyContent[i];
-}
-}
-}
-     })
+    if (selectedBtn == '') {
+        console.log("Selected Button had no value")
+        document.getElementById("invalid-choice").className = 'transition-from-hidden-content'; 
+            setTimeout(function(){ 
+                document.getElementById("invalid-choice").className = 'invisble-content'; 
+            }, 2000);
+    } else {
+        toDisplay = findValues(data, selectedBtn);
+        keyContent = Object.keys(toDisplay[0][0])
+        keyContent = keyContent.sort();
+        for (var i=0; i < 10; i++) {
+            document.getElementById("optn-btn-ttl-" + i).classList = 'optn-btn-ttl invisible-content';
+        }}
+        setTimeout(function() {
+            if (keyContent != "ARTICLE") {
+                for (var i=0; i < keyContent.length; i++) {
+                    document.getElementById('optn-btn-ttl-' + i).innerHTML = keyContent[i];
+                }
+                var numOfBoxToClear = 10 - keyContent.length;
+                for (var i = keyContent.length; i< numOfBoxToClear; i++) {
+                    document.getElementById('optn-btn-ttl-' + i).innerHTML = '';
+                }
+                document.getElementById("choice-title").innerHTML = '';
+            } else {
+                console.log(keyContent[0])
+            }
+        }, 600);
+        setTimeout(function(){ 
+            for (var i=0; i < 10; i++) {
+                document.getElementById("optn-btn-ttl-" + i).className = 'transition-from-hidden-content'; 
+            }
+        }, 600);
+    } else if (pass == 'hover') {
+        var hoverBtn = document.getElementById('optn-btn-ttl-' + optnhvrchc).innerHTML;
+        document.getElementById("choice-hover-title").innerHTML = hoverBtn;
+        toDisplay = findValues(data, hoverBtn);
+        keyContent = Object.keys(toDisplay[0][0])
+        keyContent = keyContent.sort();
+        var keyContentSubtract = keyContent.length - 1;
+        for (var i = 0; i < keyContent.length; i++) {
+            var choiceHoverList = document.getElementById("choice-hover-list");
+            if (keyContentSubtract != i && i > 0) {
+                choiceHoverList.innerHTML = choiceHoverList.innerHTML += ', ' + keyContent[i];
+            } else if (i == 0) {
+                choiceHoverList.innerHTML = keyContent[i];
+            } else  if (keyContentSubtract == i) {
+                if (keyContent.length > 2) {
+                choiceHoverList.innerHTML = choiceHoverList.innerHTML += ', & ' + keyContent[i];
+                } else {
+                choiceHoverList.innerHTML = choiceHoverList.innerHTML += ' & ' + keyContent[i];
+                }
+            }
+        }
+document.getElementById("choice-hover-title").className = 'quick-shown-content';
+document.getElementById("choice-hover-list").className = 'quick-shown-content';
+    }
+    })
 .catch(function (err) {
     console.log(err);
   });}
 function hvrStp() {
+document.getElementById("choice-hover-title").className = 'quick-invis-content';
+document.getElementById("choice-hover-list").className = 'quick-invis-content';
 document.getElementById("choice-hover-title").innerHTML = '';
 document.getElementById("choice-hover-list").innerHTML = '';
 }
